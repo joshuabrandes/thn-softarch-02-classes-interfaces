016fffcf5ac673e9df232bdc01983e2d57b22a22
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-03-23 16:57:57 +0100

    start with SimpleListImpl

--- src/main/java/ohm/softa/a02/SimpleListImpl.java
+++ src/main/java/ohm/softa/a02/SimpleListImpl.java
@@ -1,11 +1,37 @@
 package ohm.softa.a02;
 
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.Data;
+
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
+@Data
 public class SimpleListImpl implements SimpleList {
 
+    Element head = null;
+
 	// TODO: Implement the required methods.
+
+    private static class Element {
+        Object item;
+        Element next;
+    }
+
+    class SimpleIteratorImpl implements Iterator<Element> {
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public Element next() {
+            return ;
+        }
+    }
 
 }
